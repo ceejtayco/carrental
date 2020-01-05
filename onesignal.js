ac0531ecd3d8b38eff058d1ce03711c920605925
Ceej Tayco <ceejtayco@Ceejs-MacBook-Pro.local>
2020-01-05 23:23:57 +0800

    commit

--- onesignal.js
+++ onesignal.js
@@ -5,12 +5,7 @@ OneSignal.push(function() {
     autoResubscribe: true,
     notifyButton: {
       enable: true,
-    },
-    welcomeNotification: {
-      "title": "My Custom Title",
-      "message": "Thanks for subscribing!",
-      // "url": "" /* Leave commented for the notification to not open a window on Chrome and Firefox (on Safari, it opens to your webpage) */
     }
   });
-  OneSignal.showNativePrompt();
+  
 });
