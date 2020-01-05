29c772941f38d670632c97b8c1e2642df22cf964
Ceej Tayco <ceejtayco@Ceejs-MacBook-Pro.local>
2020-01-05 22:48:11 +0800

    commit changes

--- index.php
+++ index.php
@@ -46,12 +46,15 @@ error_reporting(0);
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
 <link rel="stylesheet" href="assets/css/modal_confirmation.css">
-<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"></script>
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
 <script>
   var OneSignal = window.OneSignal || [];
   OneSignal.push(function() {
     OneSignal.init({
       appId: "c7203da0-332c-4ab4-bf61-9e3802b93cb8",
+      notifyButton: {
+        enable: true,
+      },
     });
   });
 </script>
