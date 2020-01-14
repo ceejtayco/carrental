426356c88ea4f124b318c8dcb689aab939c374bb
Ceej Tayco <ceejtayco@Ceejs-MacBook-Pro.local>
2020-01-14 23:57:39 +0800

    change

--- includes/header.php
+++ includes/header.php
@@ -62,7 +62,7 @@ foreach($results as $result)
     if($_SESSION['utype'] == '0' && !empty($_SESSION['verified_at'])){
   ?>
 <i class="fa fa-check-circle" aria-hidden="true" style="color: skyblue; font-size: 15px;"></i>
-<input type="text" name="session_user_name" id="session_user_name" value="<?php echo $_SESSION['login']; ?>"></input>
+<h1 id="session_user_name" name="session_user_name"><?php echo $_SESSION['login']; ?></h1>
 <input type="hidden" name="session_user_id" id="session_user_id" value="<?php echo $result->id; ?>"></input>
 <input type="hidden" name="session_user_type" id="session_user_type" value="<?php echo $result->userType; ?>"></input>
   <?php
